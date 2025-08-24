a4f9106430d8fa5f32ddb04b237ca040c2157b45
Akash Singh <akashgurjar6266@gmail.com>
2025-08-24 22:49:26 +0530

    Added a new Role entity

---
+++ src/main/java/com/exam/examserver/model/Role.java
@@ -0,0 +1,36 @@
+package com.exam.examserver.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Role {
+	
+	@Id
+	private Long roleId;
+	private String roleName;
+	public Long getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	public Role(Long roleId, String roleName) {
+		super();
+		this.roleId = roleId;
+		this.roleName = roleName;
+	}
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
