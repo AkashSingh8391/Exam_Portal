fdf0cfa94f375fe8154e9c9ad8852ed55b76d27a
Akash Singh <akashgurjar6266@gmail.com>
2025-08-25 00:48:21 +0530

    Added a repo file for performing some database operation

--- src/main/java/com/exam/examserver/model/Role.java
+++ src/main/java/com/exam/examserver/model/Role.java
@@ -1,14 +1,39 @@
 package com.exam.examserver.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Role {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long roleId;
 	private String roleName;
+	
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="role")
+	@JsonIgnore
+	private Set<UserRole> userRoles=new HashSet<>();
+	
+	
+	
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
 	public Long getRoleId() {
 		return roleId;
 	}
--- src/main/java/com/exam/examserver/model/User.java
+++ src/main/java/com/exam/examserver/model/User.java
@@ -1,9 +1,17 @@
 package com.exam.examserver.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -116,6 +124,17 @@ public class User {
 					this.enabled = enabled;
 				}
 				
+				@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER, mappedBy="user")
+				@JsonIgnore
+				private Set<UserRole> userRoles=new HashSet<>();
+
+				public Set<UserRole> getUserRoles() {
+					return userRoles;
+				}
+
+				public void setUserRoles(Set<UserRole> userRoles) {
+					this.userRoles = userRoles;
+				}
 				
 				
 }
